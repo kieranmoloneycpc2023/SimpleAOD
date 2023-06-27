606de9c0348a53e2bb3fe46446419ac54bde36ef
kieranmoloneycpc2023 <137910243+kieranmoloneycpc2023@users.noreply.github.com>
2023-06-27 11:07:55 -0400

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,1 +1,12 @@
+// Display selected file names in the form
+const fileInput = document.querySelector('input[name="pdf_files"]');
+const fileInputLabel = document.querySelector('.file-input-label');
 
+fileInput.addEventListener('change', () => {
+    if (fileInput.files.length > 0) {
+        const fileNames = Array.from(fileInput.files).map(file => file.name);
+        fileInputLabel.textContent = fileNames.join(', ');
+    } else {
+        fileInputLabel.textContent = 'Choose PDF files';
+    }
+});
